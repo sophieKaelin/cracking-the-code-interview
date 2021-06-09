9902d38d4b714dd79ddcf371c026d4ecd38814d5
Sophie Kaelin <48739273+sophieKaelin@users.noreply.github.com>
2021-06-09 16:54:20 +1000

    Remove test functions

--- arraysAndStrings.py
+++ arraysAndStrings.py
@@ -7,12 +7,6 @@ def isUnique(string):
             return False
     return True
 
-def test_isUnique():
-    assert isUnique("abcdefg") == True, "Should be True"
-    assert isUnique("abcdefa") == False, "Should be False"
-    assert isUnique("") == True, "Should be True when empty string"
-    assert isUnique("a") == True, "Should be True if single character"
-
 # 1.2 Check Permutation: Given two strings, write a method to decide if one is a permutation of the other
 def isPermutation(string1, string2):
     if len(string1) == len(string2) == 0:
@@ -24,17 +18,6 @@ def isPermutation(string1, string2):
     else:
         return False
 
-def test_isPermutation():
-    assert isPermutation("god", "dog") == True, "Bad and Dog ARE permutations"
-    assert isPermutation("cat", "dog") == False, "cat and Dog ARE NOT permutations"
-    assert isPermutation("racecar", "acecarr") == True, "Should be True"
-    assert isPermutation("racecar", "acecar") == False, "Should be False"
-    assert isPermutation("", "") == True, "Should be True"
-    assert isPermutation("", "acecarr") == False, "Should be False"
-    assert isPermutation("racecar", "") == False, "Should be False"
-    assert isPermutation("race car", "acecarr ") == True, "Should be true"
-    assert isPermutation("dog", "dog") == True, "Should be true"
-
 # 1.3 URLify: Write a method to replace all spaces in a string with %20
 def URLify(string):
     # SIMPLE: return string.replace(' ', '%20')
@@ -45,13 +28,6 @@ def URLify(string):
         else:
             newstring += ch 
     return newstring
-
-def test_URLify():
-    assert URLify("Hello World") == "Hello%20World", "Should be Hello%20World"
-    assert URLify("Hello  World") == "Hello%20%20World", "Should be Hello%20%20World"
-    assert URLify("HelloWorld") == "HelloWorld", "Should be HelloWorld"
-    assert URLify("%20 %20ab") == "%20%20%20ab", "Should be %20%20%20ab"
-    assert URLify("") == "", ""
 
 # 1.4 Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palindrome. Permutation is rearangment of letters
 def palindrome(string):
@@ -68,19 +44,3 @@ def palindrome(string):
         if countOdds > 1:
             return False
     return True
-
-def test_palindrome():
-    assert palindrome("racecar") == True
-    assert palindrome("racecra") == True
-    assert palindrome("raceca") == False
-    assert palindrome("") == True
-    assert palindrome("a") == True
-    assert palindrome("abc") == False
-
-if __name__ == "__main__":
-    # Write tests here
-    test_isUnique()
-    test_isPermutation()
-    test_URLify()
-    test_palindrome()
-    print("Everything passed")
